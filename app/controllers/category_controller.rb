64ee2c28c895e3c2b3789201fcb1ae3a96db879a
Ashish Singh <singhashish4000@gmail.com>
2018-08-27 01:23:22 +0530

    Removed  hardcoded id

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,7 +2,7 @@ class CategoryController < ApplicationController
   def index
     @category_col_one = Category.find([1,2,3,4,5])
     @category_col_two = Category.find([6,7,8,9,10])
-    @category_col_three = Category.find([11,12,13,14,15])
+    @category_col_three = Category.find([11,12])
     @category = Category.all
   end
 
