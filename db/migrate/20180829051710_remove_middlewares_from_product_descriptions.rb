cd74800ea3c8f1762a0a4bbab1cc3f59b147d794
Ashish Singh <singhashish4000@gmail.com>
2018-08-29 10:55:09 +0530

    changed name

--- db/migrate/20180829051710_remove_middlewares_from_product_descriptions.rb
+++ db/migrate/20180829051710_remove_middlewares_from_product_descriptions.rb
@@ -1,5 +1,5 @@
 class RemoveMiddlewaresFromProductDescriptions < ActiveRecord::Migration
   def change
-    remove_reference :product_descriptions, :middlewares, index: true, foreign_key: true
+    remove_reference :product_descriptions, :middlewaress, index: true, foreign_key: true
   end
 end
